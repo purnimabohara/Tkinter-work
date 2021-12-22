c0d91da98505a1ef22f83aa73f66ba46a51906d7
purnimabohara <purnimabohara6@gmail.com>
2021-12-22 18:48:16 +0545

    tkinterlab

---
+++ Registration page/grid.py
@@ -0,0 +1,13 @@
+from tkinter import*
+
+root= Tk()
+
+a=Label(root,text="Username")
+e1=Entry(root)
+b=Label(root,text="First name")
+e2=Entry(root)
+c=Label(root,text="Last name")
+e3=Entry(root)
+d=Label(root,text="Password")
+e4=Entry(root)
+f=Label(root,text="Confirm Password")
---
+++ Registration page/pack.py
@@ -0,0 +1,37 @@
+from tkinter import*
+
+root= Tk()
+
+a= Label(root,text="Username")
+e1= Entry(root)
+b= Label(root,text="First name")
+e2= Entry(root)
+c= Label(root,text="Last name")
+e3 = Entry(root)
+d = Label(root,text="Password")
+e4=Entry(root)
+f=Label(root,text="Confirm Password")
+e5=Entry(root)
+g=Label(root,text="Email")
+e6=Entry(root)
+h=Button(root,text="Register")
+
+
+a.pack()
+e1.pack()
+b.pack()
+e2.pack()
+c.pack()
+e3.pack()
+d.pack()
+e4.pack()
+f.pack()
+e5.pack()
+g.pack()
+e6.pack()
+h.pack()
+
+root.mainloop()
+
+  
+
